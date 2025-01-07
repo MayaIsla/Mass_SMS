4cefd6caa2a99b4db4804bb95272d9a232b83e20
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2025-01-07 15:08:19 -0600

    Update sampleSMS.py

--- sampleSMS.py
+++ sampleSMS.py
@@ -16,10 +16,7 @@ client = Client(account_sid, auth_token)
 
 for i in df['Phone']:
     try:
-        message = client.messages.create(
-        to = i,
-        from_="+10000000000",   
-        body = "Text you want to display")
+        message = client.messages.create(to = i, from_="+10000000000",body = "Text you want to display")
         print(message.sid)
         mess_phone = str(i)
         mess_id = str(message.sid)
